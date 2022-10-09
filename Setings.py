999b799feebe52eacb8a61509d0d8fcea46c7cd0
katko23 <catalin.coseru@isa.utm.md>
2022-10-09 16:31:42 +0300

    Sooo add cooking aparatus , and a bad rating
    Yep is 100% but with a verry bad rating !!!

--- Setings.py
+++ Setings.py
@@ -1,17 +1,17 @@
 #order :
 
-# hostName = "localhost"
-# serverPort = 27003
-#
-# serverName =  "localhost"
-# this_serverPort = 27004
-
-
-hostName = ""
+hostName = "localhost"
 serverPort = 27003
 
-serverName =  ""
+serverName =  "localhost"
 this_serverPort = 27004
+
+
+# hostName = "kitchen-c"
+# serverPort = 27003
+#
+# serverName =  "dinning-c"
+# this_serverPort = 27004
 
 nr_of_tables = 6
 nr_of_waiters = 3
