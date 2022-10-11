60c6b84e18d9956cb526c1bc38d8d2f9da1f6d58
katko23 <catalin.coseru@isa.utm.md>
2022-10-11 09:52:26 +0300

    Some changes for containerization

--- Setings.py
+++ Setings.py
@@ -1,17 +1,17 @@
 #order :
 
-hostName = "localhost"
-serverPort = 27003
-
-serverName =  "localhost"
-this_serverPort = 27004
-
-
-# hostName = "kitchen-c"
+# hostName = "localhost"
 # serverPort = 27003
 #
-# serverName =  "dinning-c"
+# serverName =  "localhost"
 # this_serverPort = 27004
+
+
+hostName = "kitchen-c"
+serverPort = 27003
+
+serverName =  "dinning-c"
+this_serverPort = 27004
 
 nr_of_tables = 6
 nr_of_waiters = 3
